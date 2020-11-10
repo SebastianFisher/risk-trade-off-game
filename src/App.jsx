16fe5e1ec1c4d42c2ac8fb05cd64d2d688d182b0
SebastianF <sbfisher01@yahoo.com>
2020-11-10 11:23:10 -0700

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ export default function App() {
                     <Route path="/game-2">
                         <GameScreen version={2} />
                     </Route>
-                    <Route path="./" >
+                    <Route path="/" >
                         <HomeScreen />
                     </Route>
                 </Switch>
