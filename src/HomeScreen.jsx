3972967084679c67e7c81ae63640688f6008f310
SebastianF <sbfisher01@yahoo.com>
2020-11-15 13:05:38 -0700

    fixing button format on home screen

--- src/HomeScreen.jsx
+++ src/HomeScreen.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import {Link} from "react-router-dom";
-import {Button} from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { Button } from "react-bootstrap";
 import "./HomeScreen.css";
 
 
@@ -11,11 +11,11 @@ export default class HomeScreen extends React.Component {
                 <header>
                     <h1>Welcome!</h1>
                 </header>
-                <Link to="./game-1" className="ver-1-btn">
-                    <Button variant="danger"><span className="btn">Version One: MPT</span></Button>
+                <Link to="./game-1">
+                    <Button variant="danger" className="ver-1-btn"><span className="btn">Version One: MPT</span></Button>
                 </Link>
-                <Link to="./game-2" className="ver-2-btn">
-                    <Button variant="danger"><span className="btn">Version Two: I2O</span></Button>
+                <Link to="./game-2">
+                    <Button variant="danger" className="ver-2-btn"><span className="btn">Version Two: I2O</span></Button>
                 </Link>
             </div>
         );
